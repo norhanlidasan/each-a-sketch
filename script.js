e2d9c3a6cf7a0db114a5166f513c96758215de28
norhanlidasan <177215771+norhanlidasan@users.noreply.github.com>
2024-09-23 14:34:06 +0300

    Add function for each a sketch

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const container = document.querySelector(".container");
 
-let pixel = 16;
+// function for the pixels (divs)
 
-let heightPixel = 500 / pixel;
-let widthPixel = 500 / pixel;
-
+let heightPixel; // global declare of submit button
+let widthPixel; // global declare of submit button
 let createDiv = (size) => { 
   for (let i = 0; i < size * size; i++) {
     let colDiv = document.createElement("div");
@@ -12,19 +11,57 @@ let createDiv = (size) => {
     colDiv.style.height = `${heightPixel}px`;
     colDiv.style.width = `${widthPixel}px`;
     container.appendChild(colDiv);
-
   }
 }
 
+const submit = document.querySelector("#submit");
+let input = document.querySelector("#myPixel");
 
-createDiv(pixel);
+// evaluate the input from 2 to 100
+submit.addEventListener("click", () => {
+  if (input.value > 100) {
+    alert("You type more than 100");
+    location.reload(); 
+  } else if (input.value < 2) {
+    alert("You type more less than 2");
+  } else {
+  let pixelValue = input.value;
+  pixel = pixelValue;
+  heightPixel = 500 / pixel;
+  widthPixel = 500 / pixel;
+  createDiv(pixel);
+  pixels = document.querySelectorAll(".col"); // need to declare the pixels here
+  changeColor(); 
+  submit.disabled = true;
+  }
+})
 
-let pixels = document.querySelector(".container > div");
-
-pixels.addEventListener('mouseenter', (e) => {
-    console.log(e.type);
-    pixels.style.backgroundColor = "blue";
+let pixels;
+let pixel;
+function changeColor() {
+// for color each div
+pixels.forEach(pix => {
+    pix.addEventListener('mouseenter', (e) => {
+    console.log(e.type);   
+    // RGB colors
+    let redColor = Math.floor(Math.random() * 255);
+    let greenColor = Math.floor(Math.random() * 255);
+    let blueColor = Math.floor(Math.random() * 255);
+    pix.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor}, 0.8)`;
+    });
 });
+}
+
+// reset the sketch
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  location.reload();
+})
+
+
+
+
+
 
 
 
