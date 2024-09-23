807b73819ce8f65b062c7408578308b4e46320d2
norhanlidasan <177215771+norhanlidasan@users.noreply.github.com>
2024-09-23 14:49:07 +0300

    rearrange some indentation

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const container = document.querySelector(".container");
-
-// function for the pixels (divs)
-
 let heightPixel; // global declare of submit button
 let widthPixel; // global declare of submit button
+// function for the pixels (divs)
 let createDiv = (size) => { 
   for (let i = 0; i < size * size; i++) {
     let colDiv = document.createElement("div");
@@ -19,42 +17,43 @@ let input = document.querySelector("#myPixel");
 
 // evaluate the input from 2 to 100
 submit.addEventListener("click", () => {
+
   if (input.value > 100) {
-    alert("You type more than 100");
-    location.reload(); 
+      alert("You type more than 100");
+      location.reload(); 
   } else if (input.value < 2) {
-    alert("You type less than 2");
+      alert("You type less than 2");
   } else {
-  let pixelValue = input.value;
-  pixel = pixelValue;
-  heightPixel = 500 / pixel;
-  widthPixel = 500 / pixel;
-  createDiv(pixel);
-  pixels = document.querySelectorAll(".col"); // need to declare the pixels here
-  changeColor(); 
-  submit.disabled = true;
+      let pixelValue = input.value;
+      let pixel = pixelValue;
+      heightPixel = 500 / pixel;
+      widthPixel = 500 / pixel;
+      createDiv(pixel);
+      pixels = document.querySelectorAll(".col"); // need to declare the pixels here
+      changeColor(); 
+      submit.disabled = true;
   }
 })
 
-let pixels;
-let pixel;
+let pixels; // global declaration for submit
+
 function changeColor() {
-// for color each div
-pixels.forEach(pix => {
-    pix.addEventListener('mouseenter', (e) => {
-    console.log(e.type);   
-    // RGB colors
-    let redColor = Math.floor(Math.random() * 255);
-    let greenColor = Math.floor(Math.random() * 255);
-    let blueColor = Math.floor(Math.random() * 255);
-    pix.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor}, 0.8)`;
-    });
-});
+  // for color each div
+      pixels.forEach(pix => {
+        pix.addEventListener('mouseenter', (e) => {
+        console.log(e.type);   
+        // RGB colors
+        let redColor = Math.floor(Math.random() * 255);
+        let greenColor = Math.floor(Math.random() * 255);
+        let blueColor = Math.floor(Math.random() * 255);
+        pix.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor}, 0.8)`;
+      });
+  });
 }
 
 // reset the sketch
 let reset = document.querySelector("#reset");
-reset.addEventListener("click", () => {
+  reset.addEventListener("click", () => {
   location.reload();
 })
 
